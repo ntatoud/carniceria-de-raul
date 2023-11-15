e970bb2a133593daae6e956e7bc9fd8f07380117
SMattii <mattiasica.2001@gmail.com>
2023-11-15 14:07:18 +0100

    Registration/Login partial implementation
    Missing DB interactions. Added labels to necessary fields. Salted hashing implementation. To be shown to who's doing the DB.

--- src/features/auth/login/index.ts
+++ src/features/auth/login/index.ts
@@ -1,8 +1,24 @@
 import { Router, urlencoded, Request, Response } from "express";
-
+import { xorStringWith32ByteKey, sha256HexString, generate32ByteSalt } from "../util";
 const router = Router();
 
 router.use(urlencoded({ extended: true }));
+
+// Define a route for handling POST requests to the "/login" endpoint
+router.post('/log', (req : Request, res : Response) => {
+  // Access submitted data from the request body
+  const { email, password } = req.body;
+//Send email to server and request associated salt
+//Calculate the xor of password with salt
+//SHA256 of the result of the xor
+//Send to server email and saltedhashpwd
+
+//  if (email === 'exampleUser' && password === 'examplePassword') {
+//    res.status(200).json({ message: 'Login successful' });
+//  } else {
+//    res.status(401).json({ message: 'Invalid username or password' });
+//  }
+});
 
 router.use("/", (req: Request, res: Response) => {
   res.render("login.ejs");
--- src/features/auth/signup/index.ts
+++ src/features/auth/signup/index.ts
@@ -1,8 +1,19 @@
 import { Router, urlencoded, Request, Response } from "express";
+import { generate32ByteSalt, xorStringWith32ByteKey, sha256HexString } from "../util";
 
 const router = Router();
 
 router.use(urlencoded({ extended: true }));
+
+// Define a route for handling POST requests to the "/login" endpoint
+router.post('/sign', (req : Request, res : Response) => {
+  const {name, surname, email, password} = req.body;
+  // Access submitted data from the request body
+  const salt = generate32ByteSalt();
+  const saltedPwd = xorStringWith32ByteKey(password, salt);
+  const hashPwd = sha256HexString(saltedPwd);
+  // Send to DB name, surname, email, hashPwd
+});
 
 router.use("/", (req: Request, res: Response) => {
   res.render("signup.ejs");
