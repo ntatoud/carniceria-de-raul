d59948b8657e62ebec0df4693df773f2aa74bc6c
Noé <noe.tatoud@bearstudio.fr>
2023-11-20 15:55:13 +0100

    feat: Create base for admin user layout

--- src/features/admin/users/index.ts
+++ src/features/admin/users/index.ts
@@ -4,8 +4,19 @@ const router = Router();
 
 router.use(urlencoded({ extended: true }));
 
+type User = {
+  name: string;
+  id: string;
+  email: string;
+  status: string;
+  authorities: boolean;
+};
+
+const dataFromBack: User[] = [
+  { name: "TRUC", id: "10", email: "dad", status: "oui", authorities: true },
+];
 router.use("/", (req: Request, res: Response) => {
-  res.render("users.ejs");
+  res.render("users.ejs", { dataToFront: dataFromBack });
 });
 
 export default router;
