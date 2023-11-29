a22d27e9e18731185e98806cad373358bc7dac85
Ruben Francisco Artero Banares <rab400@inlumine.ual.es>
2023-11-29 21:26:07 +0900

    Error correction

--- src/features/account/password/index.ts
+++ src/features/account/password/index.ts
@@ -2,10 +2,12 @@ import { Router, Response, Request } from "express";
 
 const router = Router();
 
+
 router.use("/", (req: Request, res: Response) => {
   res.render("password.ejs",{
     accountName: "Nombre de usuario",
   });
 });
+
 
 export default router;
