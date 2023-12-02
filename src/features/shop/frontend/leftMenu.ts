76a58e378a77483a00efbeb69046b35fb7e37aa3
Noé <noe.tatoud@bearstudio.fr>
2023-12-02 17:59:25 +0100

    feat: Put scripts in TS files and cleaned the code

---
+++ src/features/shop/frontend/leftMenu.ts
@@ -0,0 +1,34 @@
+const toggleLeftMenu = (event: MouseEvent) => {
+  $(event.currentTarget!).parents(".left-menu").toggleClass("collapse");
+};
+
+const loadSwitch = (name: string, params: string[]) => {
+  $(`.form-switch input[name='${name}']`).prop(
+    "checked",
+    params.includes(`${name}=on`)
+  );
+};
+
+const turnOtherSwitchOff = (event: MouseEvent) => {
+  if ($(event.currentTarget!).prop("checked")) {
+    const switchName = $(event.currentTarget!).attr("name");
+
+    const otherSwitch = $(
+      `.form-switch input[name='${switchName === "price" ? "name" : "price"}']`
+    );
+    otherSwitch.prop("checked", false);
+  }
+};
+
+const params = window.location.search.replace("?", "").split("&");
+loadSwitch("ofertas", params);
+loadSwitch("price", params);
+loadSwitch("name", params);
+
+// Desactivate previous item
+$(".left-menu-categories .left-menu-items.active").removeClass("active");
+
+// Activate new one
+$(
+  `.left-menu-categories .left-menu-item[href='${window.location.pathname}']`
+).addClass("active");
---
+++ src/features/shop/shopHome.ts
@@ -0,0 +1,41 @@
+// Home javascript
+const scrollToRight = () => {
+  scrollPos +=
+    window.screen.width <= mobileScreenWidth ? mobileScroll : desktopScroll;
+  $(`.shop-products-container:not(.hide)`).scrollLeft(scrollPos);
+};
+const scrollToLeft = () => {
+  scrollPos -=
+    window.screen.width <= mobileScreenWidth ? mobileScroll : desktopScroll;
+  $(`.shop-products-container:not(.hide)`).scrollLeft(scrollPos);
+};
+
+const setScroll = () => {
+  scrollPos = $(`.shop-products-container:not(.hide)`).scrollLeft() ?? 0;
+};
+
+const switchTab = (event: MouseEvent) => {
+  const tab = event.target!;
+  const tabId = $(tab).attr("id");
+  const otherTab = $(`button#${tabId === "ofertas" ? "mejores" : "ofertas"}`);
+  const otherTabId = $(otherTab).attr("id");
+
+  // Display the products related to the tab that has been clicked and activate the tab
+  $(tab).addClass("active");
+  $(`.shop-products-container.${tabId}`).show();
+  $(`.shop-products-container.${tabId}`).removeClass("hide");
+
+  // Remove the products related to the other tab and desactivate the tab
+  $(otherTab).removeClass("active");
+  $(`.shop-products-container.${otherTabId}`).addClass("hide");
+  $(`.shop-products-container.${otherTabId}`).hide();
+
+  // Reset the scroll for the new tab
+  scrollPos = 0;
+};
+
+const mobileScreenWidth = 768;
+const mobileScroll = 256;
+const desktopScroll = 400;
+$(`.shop-products-container.mejores`).hide();
+let scrollPos = 0;
