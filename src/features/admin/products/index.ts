0c2754491a7d355ddebef695157f30a421d6c5c9
Noé <noe.tatoud@bearstudio.fr>
2023-12-03 21:56:53 +0100

    feat: CRUD Products + Form layout

--- src/features/admin/products/index.ts
+++ src/features/admin/products/index.ts
@@ -1,11 +1,39 @@
 import { Router, urlencoded, Request, Response } from "express";
+import {
+  getProductList,
+  getProductToUpdate,
+  productCreate,
+  productDelete,
+  productUpdate,
+} from "./utils";
 
 const router = Router();
 
 router.use(urlencoded({ extended: true }));
 
+router.post("/delete/:id", (req: Request, res: Response) => {
+  const productId = req.params.id;
+  productDelete(res, productId);
+});
+
+router.post("/create", (req: Request, res: Response) => {
+  productCreate(res, req.body);
+});
+
+router.use("/create", (req: Request, res: Response) => {
+  res.render("productCreate.ejs", { product: undefined });
+});
+
+router.post("/update/:id", (req: Request, res: Response) => {
+  productUpdate(res, req.body, req.params.id);
+});
+
+router.use("/update/:id", (req: Request, res: Response) => {
+  getProductToUpdate(res, req.params.id);
+});
+
 router.use("/", (req: Request, res: Response) => {
-  res.render("products.ejs");
+  getProductList(res);
 });
 
 export default router;
