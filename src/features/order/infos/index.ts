33015b0bd778b5494ad7f423f266feda84bbdaeb
Noé <noe.tatoud@bearstudio.fr>
2023-12-12 20:14:17 +0100

    fix: Form flow and required fields

--- src/features/order/infos/index.ts
+++ src/features/order/infos/index.ts
@@ -4,6 +4,9 @@ const router = Router();
 
 router.use(urlencoded({ extended: true }));
 
+router.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+});
 router.use("/", (req: Request, res: Response) => {
   res.render("infos.ejs");
 });
