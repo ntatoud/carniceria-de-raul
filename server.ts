b66ace9ab7d8358449821ee472cc319d56312a59
Ruben Francisco Artero Banares <rab400@inlumine.ual.es>
2023-12-28 20:33:34 +0900

    Payment with stripe by creating our own form

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+/*
 import express, { Request, Response, static as staticSrc } from 'express';
 import i18next from '@/lib/i18n/config.js';
 import i18nextMiddleware from 'i18next-http-middleware';
@@ -101,3 +102,110 @@ app.get('*', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
+*/
+import express, { Request, Response, static as staticSrc } from 'express';
+import Stripe from 'stripe';
+import i18next from '@/lib/i18n/config.js';
+import i18nextMiddleware from 'i18next-http-middleware';
+import session from '@/lib/session/config.js';
+import bodyParser from 'body-parser';
+import auth from '@/features/auth/index.js';
+import admin from '@/features/admin/index.js';
+import order from '@/features/order/index.js';
+import shop from '@/features/shop/index.js';
+import about from '@/features/about/index.js';
+import contact from '@/features/contact/index.js';
+import account from '@/features/account/index.js';
+import { toastDispatch } from '@/components/toast/index.js';
+import { AVAILABLE_LANGUAGES } from '@/lib/i18n/constants.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const app = express();
+const port = process.env.PORT || 3000; // Use port 3000 if PORT is not defined
+
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(session);
+app.use(i18nextMiddleware.handle(i18next));
+app.use(staticSrc('public'));
+app.use(staticSrc('public'));
+app.use(staticSrc('dist/src'));
+app.use(staticSrc('node_modules/bootstrap/dist/'));
+app.use(staticSrc('node_modules/@popperjs/core/dist/umd'));
+app.use(staticSrc('node_modules/jquery/dist/'));
+app.use(staticSrc('node_modules/bootstrap-icons/font'));
+
+
+app.set('view engine', 'ejs');
+
+app.set('views', [
+  'src/features',
+  'src/features/auth',
+  'src/features/auth/login',
+  'src/features/auth/signup',
+  'src/features/auth/reset',
+  'src/features/admin',
+  'src/features/admin/products',
+  'src/features/admin/users',
+  'src/features/admin/orders',
+  'src/features/order',
+  'src/features/order/cart',
+  'src/features/order/infos',
+  'src/features/order/payment',
+  'src/features/shop',
+  'src/features/shop/_partials',
+  'src/features/layout',
+  'src/features/about',
+  'src/features/contact',
+  'src/features/account',
+  'src/features/account/profile',
+  'src/features/account/password',
+]);
+
+// Declaration of the routes from the root of the website
+app.use('/auth', auth); // Has subroutes
+app.use('/admin', admin); // Has subroutes
+app.use('/shop', shop); // Has subroutes
+app.use('/order', order); // Has subroutes
+app.use('/about', about);
+app.use('/contact', contact);
+app.use('/account', account); // Has subroutes
+
+app.post('/lang', (req: Request, res: Response) => {
+  const langKey = req.body.langKey;
+  i18next.changeLanguage(langKey);
+  const language = AVAILABLE_LANGUAGES.find(({ key }) => key === langKey);
+
+  res.status(200).send(language);
+});
+
+app.get('/', (req: Request, res: Response) => {
+  const { isLogged, user, cart } = req.session;
+
+  res.render('index.ejs', {
+    isLogged: isLogged,
+    account: user,
+    toast: toastDispatch(req),
+    cart: cart,
+    t: i18next.t,
+  });
+});
+
+app.get('*', (req: Request, res: Response) => {
+  res.status(404);
+  res.render('404.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
+
--- src/features/order/payment/index.ts
+++ src/features/order/payment/index.ts
@@ -1,7 +1,19 @@
-import { toastDispatch } from '@/components/toast/index.js';
+import { toastSuccess,toastError,toastDispatch} from '@/components/toast/index.js';
 import { Request, Response, Router, urlencoded } from 'express';
+import env from "dotenv";
+import Stripe from "stripe";
+import { cartTotalPrice } from '../cart/utils.js';
 
+env.config();
 const router = Router();
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error('Stripe secret key is not defined in environment variables');
+}
+
+// Now create the Stripe instance using the verified stripeSecretKey
+const stripe = new Stripe(stripeSecretKey);
 
 router.use(urlencoded({ extended: true }));
 
@@ -12,6 +24,27 @@ router.use('/', (req: Request, res: Response) => {
     cart: req.session.cart,
     toast: toastDispatch(req),
   });
+
+});
+router.post('/process-payment', async (req, res) => {
+  const { payment_method_id, client_name } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      payment_method: payment_method_id,
+      amount: (Number) (cartTotalPrice(req.session.cart))*100, // Amount in cents
+      currency: 'eur',
+      description: 'Payment done by : ' + client_name,
+    });
+    req.session.toast = toastSuccess({content: 'Payment successful'});
+    res.status(200).json({ client_secret: paymentIntent.client_secret });
+
+  } catch (error) {
+    req.session.toast = toastError({content: 'Payment failed'});
+    res.status(500).json({ error: "There was a problem during the transaction" });
+    
+  }
+  res.status(401).redirect('/shop');
 });
 
 export default router;
